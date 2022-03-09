fd0b470d024060ddf1f6ced65009f27538227f13
Aquib69 <aaquib.rafique@gmail.com>
2022-03-09 18:50:23 +0500

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:myapp_catalog/home_page.dart';
+import 'package:myapp_catalog/pages/home_page.dart';
+import 'package:myapp_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+
+      // ignore: prefer_const_literals_to_create_immutables
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: use_key_in_widget_constructors
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Aquib";
@@ -8,13 +9,16 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // ignore: prefer_const_constructors
         title: Text("Pak Tour Guide & Services"),
       ),
       body: Center(
+        // ignore: avoid_unnecessary_containers
         child: Container(
           child: Text("Welcome to $days days of Flutters by $name "),
         ),
       ),
+      // ignore: prefer_const_constructors
       drawer: Drawer(),
     );
   }
